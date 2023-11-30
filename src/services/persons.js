315f83d32b09ecfc862ba2c84192187cec2e39b5
Markeesi <62094844+Markeesi@users.noreply.github.com>
2023-11-30 18:11:10 +0200

    base URL change again and again and new headers for axios new update

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,31 +1,54 @@
 import axios from "axios";
-// const baseUrl = "http://localhost:3001/persons";
+
 const baseUrl = "https://my-json-server.typicode.com/Markeesi/puhelinluettelo-api/persons";
 
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+  headers: {
+    "Cache-Control": "no-cache",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Access-Control-Allow-Origin": "*",
+  }
+});
 
-const getAll = () => {
-  const request = axios.get(baseUrl,  {
-    headers: {
-      "Cache-Control": "no-cache",
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Access-Control-Allow-Origin": "*",
-    },})
-  return request.then((response) => response.data);
+const getAll = async () => {
+  try {
+    const response = await axiosInstance.get("");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 };
 
-const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject);
-  return request.then((response) => response.data);
+const create = async (newObject) => {
+  try {
+    const response = await axiosInstance.post("", newObject);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating data:", error);
+    throw error;
+  }
 };
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data);
+const update = async (id, newObject) => {
+  try {
+    const response = await axiosInstance.put(`/${id}`, newObject);
+    return response.data;
+  } catch (error) {
+    console.error("Error updating data:", error);
+    throw error;
+  }
 };
 
-const deletePerson = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then((response) => response.data);
+const deletePerson = async (id) => {
+  try {
+    const response = await axiosInstance.delete(`/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting data:", error);
+    throw error;
+  }
 };
 
 export default { getAll, create, update, deletePerson };
